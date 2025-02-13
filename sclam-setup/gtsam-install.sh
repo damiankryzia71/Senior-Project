f759875cfb70b2e2b9ba62cd9c1b8bb04f3fca71
damiankryzia71 <79245828+damiankryzia71@users.noreply.github.com>
2025-02-13 15:28:00 -0800

    Update gtsam-install.sh

--- sclam-setup/gtsam-install.sh
+++ sclam-setup/gtsam-install.sh
@@ -1,19 +1,10 @@
+cd ~
 sudo apt update
 sudo apt-get install libboost-all-dev
 sudo apt-get install cmake
-
 git clone -b 4.1 https://github.com/borglab/gtsam.git
 cd gtsam
 mkdir build
 cd build
-cmake ..
+sudo cmake ..
 sudo make install
-
-echo "GTSAM successfully installed. Reboot now? (Y/n): "
-read answer
-if [["$answer" == "y" || "$answer" == "Y"] || -z "$answer"]; then
-    echo "Rebooting now..."
-    sudo reboot
-else
-    echo "Reboot canceled."
-fi

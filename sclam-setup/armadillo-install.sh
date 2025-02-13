719f584ab21a6bc21eae00fc469718a7771c61cf
damiankryzia71 <79245828+damiankryzia71@users.noreply.github.com>
2025-02-13 14:44:48 -0800

    Update armadillo-install.sh

--- sclam-setup/armadillo-install.sh
+++ sclam-setup/armadillo-install.sh
@@ -1,14 +1,11 @@
+# first, download Armadillo 11.0.1 from
+# https://sourceforge.net/projects/arma/files/armadillo-11.0.1.tar.xz/download?use_mirror=master
+# then run this script
+
 sudo apt update
 sudo apt install cmake libopenblas-dev liblapack-dev libarpack2-dev libsuperlu-dev
+cd ~
+sudo mv Downloads/armadillo-11.0.1.tar.xz .
 sudo tar xf armadillo-11.0.1.tar.xz
 cd armadillo-11.0.1/
-cmake .
-
-echo "Armadillo successfully installed. Reboot now? (Y/n): "
-read answer
-if [["$answer" == "y" || "$answer" == "Y"] || -z "$answer"]; then
-    echo "Rebooting now..."
-    sudo reboot
-else
-    echo "Reboot canceled."
-fi
+sudo cmake .

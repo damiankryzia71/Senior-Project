2dbbed1a2782f244f8c14504bcc3540f5e6c4acf
damiankryzia71 <79245828+damiankryzia71@users.noreply.github.com>
2025-02-14 00:02:58 -0800

    Update opencv-install.sh

--- sclam-setup/opencv-install.sh
+++ sclam-setup/opencv-install.sh
@@ -1,14 +1,13 @@
 cd ~
 sudo apt-get install build-essential
-sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev g++ wget unzip
 sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
-wget https://github.com/opencv/opencv/archive/4.2.0.zip
-wget https://github.com/opencv/opencv_contrib/archive/refs/heads/4.x.zip
-sudo unzip 4.2.0.zip
-sudo unzip 4.x.zip
-cd opencv-4.2.0
-sudo mkdir build
-cd build
-sudo cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.x/modules ..
+sudo wget -O opencv.zip https://github.com/opencv/opencv/archive/4.x.zip
+sudo wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.x.zip
+sudo unzip opencv.zip
+sudo unzip opencv_contrib.zip
+sudo mkdir opencv-build
+cd opencv-build
+sudo cmake -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib-4.x/modules ../opencv-4.x
 sudo make -j$(nproc)
 sudo make install

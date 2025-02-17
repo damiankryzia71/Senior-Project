8d92788a95f5ef501b2227016de4eec81a307543
damiankryzia71 <79245828+damiankryzia71@users.noreply.github.com>
2025-02-16 18:24:07 -0800

    Update gtsam-install.sh

--- sclam-setup/gtsam-install.sh
+++ sclam-setup/gtsam-install.sh
@@ -4,7 +4,6 @@ sudo apt-get install libboost-all-dev
 sudo apt-get install cmake
 git clone -b 4.1 https://github.com/borglab/gtsam.git
 cd gtsam
-mkdir build
-cd build
+sudo mkdir build && cd build
 sudo cmake ..
 sudo make install
